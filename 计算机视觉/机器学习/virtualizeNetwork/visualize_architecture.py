3f95f7f71494a75672a6162463ebcd532e74a70b
周平@测试部 <195527444@qq.com>
2019-11-12 07:27:10 +0800

    Create visualize_architecture.py

---
+++ 计算机视觉/机器学习/virtualizeNetwork/visualize_architecture.py
@@ -0,0 +1,13 @@
+ '''
+apt install graphviz
+pip install graphviz==0.5.2
+pip install pydot-ng==1.0.0
+'''
+
+from pyimagesearch.nn.conv.lenet import LeNet
+from keras.utils import plot_model
+
+# initialize LeNet and then write the network arch visualization
+# graph to disk.
+model = LeNet.build(28, 28, 1, 10)
+plot_model(model, to_file="lenet.png", show_shapes=True)
