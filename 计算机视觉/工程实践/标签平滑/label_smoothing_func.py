e78060699ca559221b0402614251ee27a3525c1f
周平@测试部 <195527444@qq.com>
2020-01-10 13:26:37 +0800

    Create label_smoothing_func.py

---
+++ 计算机视觉/工程实践/标签平滑/label_smoothing_func.py
@@ -0,0 +1,120 @@
+# USAGE
+# python label_smoothing_func.py
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from pyimagesearch.learning_rate_schedulers import PolynomialDecay
+from pyimagesearch.minigooglenet import MiniGoogLeNet
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+def smooth_labels(labels, factor=0.1):
+        # smooth the labels
+        labels *= (1 - factor)
+        labels += (factor / labels.shape[1])
+
+        # returned the smoothed labels
+        return labels
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--smoothing", type=float, default=0.1,
+        help="amount of label smoothing to be applied")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+        help="path to output plot file")
+args = vars(ap.parse_args())
+
+# define the total number of epochs to train for initial learning
+# rate, and batch size
+NUM_EPOCHS = 70
+INIT_LR = 5e-3
+BATCH_SIZE = 64
+
+# initialize the label names for the CIFAR-10 dataset
+labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog",
+        "frog", "horse", "ship", "truck"]
+
+# load the training and testing data, converting the images from
+# integers to floats
+print("[INFO] loading CIFAR-10 data...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float")
+testX = testX.astype("float")
+
+# apply mean subtraction to the data
+mean = np.mean(trainX, axis=0)
+trainX -= mean
+testX -= mean
+
+# convert the labels from integers to vectors, converting the data
+# type to floats so we can apply label smoothing
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+trainY = trainY.astype("float")
+testY = testY.astype("float")
+
+# apply label smoothing to the *training labels only*
+print("[INFO] smoothing amount: {}".format(args["smoothing"]))
+print("[INFO] before smoothing: {}".format(trainY[0]))
+trainY = smooth_labels(trainY, args["smoothing"])
+print("[INFO] after smoothing: {}".format(trainY[0]))
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True,
+        fill_mode="nearest")
+
+# construct the learning rate scheduler callback
+schedule = PolynomialDecay(maxEpochs=NUM_EPOCHS, initAlpha=INIT_LR,
+        power=1.0)
+callbacks = [LearningRateScheduler(schedule)]
+
+# initialize the optimizer and model
+print("[INFO] compiling model...")
+opt = SGD(lr=INIT_LR, momentum=0.9)
+model = MiniGoogLeNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+        metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit_generator(
+        aug.flow(trainX, trainY, batch_size=BATCH_SIZE),
+        validation_data=(testX, testY),
+        steps_per_epoch=len(trainX) // BATCH_SIZE,
+        epochs=NUM_EPOCHS,
+        callbacks=callbacks,
+        verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=BATCH_SIZE)
+print(classification_report(testY.argmax(axis=1),
+        predictions.argmax(axis=1), target_names=labelNames))
+
+# construct a plot that plots and saves the training history
+N = np.arange(0, NUM_EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["acc"], label="train_acc")
+plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
